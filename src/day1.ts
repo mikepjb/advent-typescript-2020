9fd76f4a3245a7087d7614107dee8ea90a8c642d
Mike Bruce <mikepjb@gmail.com>
2020-12-06 23:53:25 +0000

    problem 1 complete!

--- src/day1.ts
+++ src/day1.ts
@@ -5,4 +5,13 @@ const expenseTotals: number[] = fs.readFileSync('data/day1-input.txt')
   .split('\n')
   .map(t => parseInt(t))
 
+expenseTotals.map((t1, i1) => {
+  expenseTotals.map((t2, i2) => {
+    if (i1 !== i2 && t1 + t2 == 2020) {
+      console.log('entry 1: ' + t1 + ', entry 2: ' + t2)
+      console.log('product: ' + t1 * t2)
+    }
+  })
+})
+
 console.log(expenseTotals)
