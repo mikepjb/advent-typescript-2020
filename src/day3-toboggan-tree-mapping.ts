48ed1ff801e6aaa6e43571055fc0d1f2f6ec117b
Mike Bruce <mikepjb@gmail.com>
2020-12-07 11:47:36 +0000

    day 3, part 2: more tree detection

--- src/day3-toboggan-tree-mapping.ts
+++ src/day3-toboggan-tree-mapping.ts
@@ -12,20 +12,35 @@ const treeCheck = (map: Map, x: number, y: number): boolean => {
   return map[y][x] === '#'
 }
 
-let pos: Position = {x: 0, y: 0}
-let treesFound = 0
-const slope: Slope = {right: 3, down: 1}
 
-while (pos.y < map.length) {
-  if (treeCheck(map, pos.x, pos.y)) {
-    console.log('found tree at: ' + pos.x + pos.y)
-    treesFound++
+const slopeA: Slope = {right: 1, down: 1}
+const slopeB: Slope = {right: 3, down: 1}
+const slopeC: Slope = {right: 5, down: 1}
+const slopeD: Slope = {right: 7, down: 1}
+const slopeE: Slope = {right: 1, down: 2}
+
+const checkSlope = (map: Map, slope: Slope): number => {
+  let treesFound = 0
+  let pos: Position = {x: 0, y: 0}
+  while (pos.y < map.length) {
+    if (treeCheck(map, pos.x, pos.y)) {
+      console.log('found tree at: ' + pos.x + pos.y)
+      treesFound++
+    }
+    pos.x += slope.right
+    if (pos.x >= map[0].length) {
+      pos.x -= map[0].length
+    }
+    pos.y += slope.down
   }
-  pos.x += slope.right
-  if (pos.x >= map[0].length) {
-    pos.x -= map[0].length
-  }
-  pos.y += slope.down
+  return treesFound
 }
 
-console.log(treesFound)
+const treeCountA = checkSlope(map, slopeA)
+const treeCountB = checkSlope(map, slopeB)
+const treeCountC = checkSlope(map, slopeC)
+const treeCountD = checkSlope(map, slopeD)
+const treeCountE = checkSlope(map, slopeE)
+
+console.log(treeCountA, treeCountB, treeCountC, treeCountD, treeCountE)
+console.log(treeCountA * treeCountB * treeCountC * treeCountD * treeCountE)
