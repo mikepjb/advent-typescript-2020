1f202b15ae8feea434d8c3b88d2fcf3fae4560c0
Mike Bruce <mikepjb@gmail.com>
2020-12-07 14:33:14 +0000

    day 4 part 2

--- src/day4-passport-processing.ts
+++ src/day4-passport-processing.ts
@@ -1,7 +1,7 @@
 import fs from 'fs'
 
 const passportFields = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
-// 'cid' field is ignored in the first part
+const acceptedEyeColors = ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
 
 type Credentials = {
   [k: string]: any
@@ -19,6 +19,33 @@ const passports: Credentials[] = fs.readFileSync('data/input-4.txt')
     })
     return creds
   })
+
+const strictValidation = (creds: Credentials): boolean => { // for part 2
+  const byr = parseInt(creds.byr)
+  if (!(byr >= 1920 && byr <= 2002)) { return false; }
+
+  const iyr = parseInt(creds.iyr)
+  if (!(iyr >= 2010 && iyr <= 2020)) { return false; }
+
+  const eyr = parseInt(creds.eyr)
+  if (!(eyr >= 2020 && eyr <= 2030)) { return false; }
+
+  const hgt = parseInt(creds.hgt.slice(0,-2))
+  const metric = creds.hgt.slice(-2)
+  if (!(metric == 'cm' && hgt >= 150 && hgt <= 193)
+    && !(metric == 'in' && hgt >= 59 && hgt <= 76)) { return false; }
+
+  const hcl = creds.hcl
+  if (!(hcl.length === 7 && hcl[0] === '#')) { return false; }
+
+  const ecl = creds.ecl
+  if (acceptedEyeColors.indexOf(ecl) === -1) { return false; }
+
+  const pid = creds.pid
+  if (!(pid.length === 9 && /^\d+$/.test(pid))) { return false; }
+
+  return true
+}
 
 // original, bad logic - took a while for figure it was bad - returns 286
 // the reason this does not work is that you can't break a forEach loop in
@@ -47,3 +74,10 @@ let validPassports = passports.filter(hasRequiredFields)
 
 console.log(validPassports)
 console.log(validPassports.length)
+
+let strictValidationPassports = passports
+  .filter(hasRequiredFields)
+  .filter(strictValidation)
+
+console.log(strictValidationPassports)
+console.log(strictValidationPassports.length)
